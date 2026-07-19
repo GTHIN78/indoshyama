e1943943ebe33bde381b55ff4e6ad9263ef476ee
Sanjay raj <sanjayraj505@gmail.com>
2026-07-20 00:06:54 +0530

    Create breakingNews.ts

---
+++ src/config/breakingNews.ts
@@ -0,0 +1,23 @@
+import { BreakingNews } from "@/types/breaking";
+
+export const breakingNews:BreakingNews[]=[
+
+{
+id:1,
+title:"AI Revolution transforms global businesses.",
+url:"#"
+},
+
+{
+id:2,
+title:"India Startup ecosystem reaches new milestone.",
+url:"#"
+},
+
+{
+id:3,
+title:"Technology trends changing digital journalism.",
+url:"#"
+}
+
+];
