1d10fc36403281c6e8ccf3c3f3220dea9bf98ac7
Sanjay raj <sanjayraj505@gmail.com>
2026-07-21 22:00:48 +0530

    Update login.dto.ts

--- login.dto.ts
+++ login.dto.ts
@@ -1,1 +1,17 @@
 
+import {
+  IsEmail,
+  IsString,
+  MinLength,
+  MaxLength,
+} from "class-validator";
+
+export class LoginDto {
+  @IsEmail()
+  email!: string;
+
+  @IsString()
+  @MinLength(8)
+  @MaxLength(128)
+  password!: string;
+}
